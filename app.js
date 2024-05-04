d733f2fac5f79e930ebd885601b93bd459b6ab48
satriawinarah <swinarah@gmail.com>
2024-05-04 22:58:26 +0700

    enhanced

--- app.js
+++ app.js
@@ -51,13 +51,13 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-if (process.env.NODE_ENV === "PRODUCTION") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+// if (process.env.NODE_ENV === "PRODUCTION") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+//   });
+// }
 
 // Using error middleware
 app.use(errorMiddleware);
